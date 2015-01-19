fbdb9c6f76d200b35cbb1652efab3333f97fa950
jorge_sosa_hdez <jorge_sosa_hdz@yahoo.com>
2015-01-19 09:27:11 -0600

    Status class update
    Status class update

---
+++ src/org/jorge/rest/status/V1_status.java
@@ -0,0 +1,25 @@
+package org.jorge.rest.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_status {
+	
+	private static final String ver = "00.01.00";
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<p>Java Web Service</p>";
+	}
+	
+	@Path ("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Version:"+ver+"</p>";
+	}
+
+
+}
