5605a6a7732248335c7cb88f789af9102d915ef3
jorge_sosa_hdez <jorge_sosa_hdz@yahoo.com>
2015-01-22 12:48:15 -0600

    epidose6 wrap-up

--- src/org/jorge/dao/Schema.java
+++ src/org/jorge/dao/Schema.java
@@ -5,10 +5,58 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.ws.rs.QueryParam;
+
 import org.codehaus.jettison.json.JSONArray;
 import org.jorge.util.ToJSON;
 
 public class Schema {
+	
+	public int insertIntoPerson(	String id, 
+									String name, 
+									String lastname, 
+									String age, 
+									String birthday	) 
+					throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		try {
+			/*
+			 * If this was a real application, you should do data validation here
+			 * before starting to insert data into the database.
+			 * 
+			 * Important: The primary key on PC_PARTS table will auto increment.
+			 * 		That means the PC_PARTS_PK column does not need to be apart of the 
+			 * 		SQL insert query below.
+			 * 
+			 */
+			conn =  MySQLRest.getMySQLConn().getConnection();
+			query = conn.prepareStatement("insert into person " +
+					"(ID, NAME, LASTNAME, AGE, BIRTHDAY) " +
+					"VALUES ( ?, ?, ?, ?, ? ) ");
+
+			int myId = Integer.parseInt(id);
+			query.setInt(1, myId);
+			
+			query.setString(2, name);
+			query.setString(3, lastname);
+			int myAge = Integer.parseInt(age);			
+			query.setInt(4, myAge);
+			query.setString(5, birthday);
+			query.execute();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			return 500; //if a error occurs, return a 500
+		}
+		finally {
+			if (conn != null) conn.close();
+		}
+
+		return 200;
+	}
+	
 
 	public JSONArray returnAllPersons(@QueryParam("id") String id) throws Exception{
 		PreparedStatement query = null;
--- src/org/jorge/rest/inventory/V2_inventory.java
+++ src/org/jorge/rest/inventory/V2_inventory.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,10 +14,12 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 import org.jorge.dao.MySQLRest;
 import org.jorge.dao.Schema;
 import org.jorge.util.ToJSON;
+
 
 @Path("/v2/inventory")
 public class V2_inventory {
@@ -50,7 +54,7 @@ public class V2_inventory {
 	@Path("/{id}/{name}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response returnID(
+	public Response returnIDName(
 			@PathParam("id") String id,
 			@PathParam("name") String name) 
 					throws Exception{
@@ -77,5 +81,84 @@ public class V2_inventory {
 
 	}
 
+	@Path("/{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnID(
+			@PathParam("id") String id) 
+					throws Exception{
+	
+	String returnString = null;	
+	JSONArray json = null;
+	
+	try{
+		
+		if (id == null){
+			return Response.status(400).entity("Error. Specify an ID").build();
+		}
+		Schema dao =  new Schema();
+		json = dao.returnAllPersons(id);
+		returnString = json.toString();
+		
+	}catch(Exception e){
+		e.printStackTrace();	
+		return Response.status(500).entity("Error. Not able to process request").build();
+	}	
+	
+	return Response.ok(returnString).build();
+	
 
+	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPcParts(String incomingData) throws Exception {
+		
+		String returnString = null;
+		//JSONArray jsonArray = new JSONArray(); //not needed
+		Schema dao = new Schema();
+		
+		try {
+			System.out.println("incomingData: " + incomingData);
+			
+			/*
+			 * ObjectMapper is from Jackson Processor framework
+			 * http://jackson.codehaus.org/
+			 * 
+			 * Using the readValue method, you can parse the json from the http request
+			 * and data bind it to a Java Class.
+			 */
+			ObjectMapper mapper = new ObjectMapper();  
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);	
+			
+			int http_code = dao.insertIntoPerson(itemEntry.id, itemEntry.name, itemEntry.lastname, itemEntry.age, itemEntry.birthday);
+			
+			if( http_code == 200 ) {
+				//returnString = jsonArray.toString();
+				returnString = "Item inserted";
+			} else {
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+}
+
+
+
+class ItemEntry{
+	
+	public String id;
+	public String name;
+	public String lastname;
+	public String age;
+	public String birthday;
+	
 }
